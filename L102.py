6688e2bb20d64d284437449f052eb44d4be66e81
amiloserny <116841799+amiloserny@users.noreply.github.com>
2022-11-06 19:34:51 -0500

    Add files via upload

---
+++ L102.py
@@ -0,0 +1,28 @@
+#Amanda Miloserny and Cathy Doherty
+#Part 2
+dict = ["Cody", "Brown", "Julia", "Bub", "Catherine", "Doherty", "Margaret", "Dunn", "Haley", "Greene", "Isabela", "Guthrie Beckstrom", "Danielle", "Hongell", "Camryn", "Hurley", "Ellen", "Kevin", "Brenna", "Kieft", "Amanda", "Miloserny", "Kaylen", "Nyhuis", "Cadyn", "Reger", "Emily", "Rusch", "Britney", "Salazar", "Julia", "Schutz", "Christina", "Shadid", "Holly", "Skrip", "Anna", "Sullivan", "Adrianne", "Verstraete", "Sarah", "Wardlow"]
+count = {}
+
+for item in dict:
+    if item in count:
+        count[item] += 1
+    else:
+        count[item] = 1
+
+print(count)
+
+#Part 3
+cpsc_names = ["Cody", "Brown", "Julia", "Bub", "Catherine", "Doherty", "Margaret", "Dunn", "Haley", "Greene", "Isabela", "Guthrie Beckstrom", "Danielle", "Hongell", "Camryn", "Hurley", "Ellen", "Kevin", "Brenna", "Kieft", "Amanda", "Miloserny", "Kaylen", "Nyhuis", "Cadyn", "Reger", "Emily", "Rusch", "Britney", "Salazar", "Julia", "Schutz", "Christina", "Shadid", "Holly", "Skrip", "Anna", "Sullivan", "Adrianne", "Verstraete", "Sarah", "Wardlow"]
+
+dict = {}
+for char in str:
+    if char in d:
+        d[char] += 1
+    else:
+        d[char] = 1
+
+
+cpsc_names[::2]
+cpsc_names[1::2]
+
+print (char)
